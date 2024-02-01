90a71680056044d64468135e2a4948d1426e0fbb
Ricardo Antonio Gomez Villalobos <bardack134@gmail.com>
2024-02-01 16:35:55 +0900

    carros creados aleatoriamente y en movimiento

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
+import random
+import threading
 import time
-from turtle import Screen
+from turtle import Screen, Turtle
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
 
+
 #TODO: configuracion del ancho y largo de la pantalla
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.bgpic(picname="prueba.png")
+screen.bgpic(picname="background.png")
+
 screen.tracer(0)
 
 player=Player()
 
 
+
 screen.listen()
+
 
 # TODO: Turtle movements,  the turtle se mueve SOLO hacia delante precionando el boton 'up'
 screen.onkeypress(player.movement, 'Up')
@@ -22,11 +28,41 @@ screen.onkeypress(player.movement, 'Up')
 
 scoreboard=Scoreboard()
 
+
+
+
+
+total_cars=[]
+
+   
+
 game_is_on = True
+
+
+def new_car():
+    random_chance = random.randint(1,6)
+    
+    if random_chance == 1:
+        car=CarManager()    
+
+        #Agrega el carro a la lista de carros   
+        total_cars.append(car)
+        
+    
+
+
+       
 while game_is_on:
     #La pantalla se va actualizar cada 0.1 segundos, dentro de nuestro while el codigo correra cada 0.1 segundos
     time.sleep(0.1)
+    
     screen.update()
+    
+    #este cilco recorre cada carro de la lista de carro y llama al metodo movement de la class CarManager para ponerlos a mover
+    for car in total_cars:
+        car.movement()
+        
+    
     
     
     player_position=player.ycor()
@@ -39,7 +75,15 @@ while game_is_on:
         player.reset_position()
         print('el nivel del player debe subir en 1')
         scoreboard.new_player_level()
+    
+   
+    new_car()
+          
+    
         
+
+
+    
 
 
 
--- player.py
+++ player.py
@@ -1,37 +1,49 @@
-from turtle import Turtle
+from turtle import Turtle  # Importa la clase Turtle del módulo turtle.
+# Define la posición inicial de la tortuga.
+STARTING_POSITION = (0, -280)  
 
+# Define la distancia que la tortuga se moverá en cada paso.
+MOVE_DISTANCE = 10  
 
-STARTING_POSITION = (0, -280)
-MOVE_DISTANCE = 10
-FINISH_LINE_Y = 280
+# Define la posición y de la línea de meta.
+FINISH_LINE_Y = 280  
 
 #TODO:create turtle class
-class Player(Turtle):
+class Player(Turtle):  
     
-   
     #hereda del modulo Turtle
-    def __init__(self):
-        #valores de nuestra funcion inical, la que corre cuando se crea un objeto de la clase
-        super().__init__()
+    # Define el método de inicialización para la clase Player.
+    def __init__(self):  
         
-        self.penup()
+        # Llama al método de inicialización de la clase padre (Turtle).
+        super().__init__()  
         
-        self.shape('turtle')
+         # Levanta el lápiz de la tortuga para que no dibuje una línea cuando se mueve.
+        self.penup() 
         
-        self.setheading(90)
+        # Establece la forma de la tortuga a 'turtle'.
+        self.shape('turtle')  
         
-        self.setposition(STARTING_POSITION)
+        # Establece la dirección inicial de la tortuga a 90 grados (hacia arriba).
+        self.setheading(90)  
         
-    
-    def movement(self):
+        # Coloca la tortuga en la posición inicial.
+        self.setposition(STARTING_POSITION) 
         
-        self.forward(MOVE_DISTANCE)
+    # Define el método de movimiento para la clase Player.
+    def movement(self):  
+        
+        # Mueve la tortuga hacia adelante la distancia definida en MOVE_DISTANCE.
+        self.forward(MOVE_DISTANCE)  
         
         
     #TODO:cuando la tortuga cruza la carretera vuelve a su posicion de origen en la parte de abajo
-    def reset_position(self):
-        
-        if self.ycor()==FINISH_LINE_Y:
-            self.goto(STARTING_POSITION)    
-        
+    # Define el método para resetear la posición de la tortuga.
+    def reset_position(self):  
+        # Si la tortuga ha cruzado la línea de meta.
+        if self.ycor()==FINISH_LINE_Y:  
+            
+            # Mueve la tortuga de vuelta a la posición inicial.
+            self.goto(STARTING_POSITION) 
+
     
